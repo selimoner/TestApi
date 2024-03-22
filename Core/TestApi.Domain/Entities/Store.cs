418f83de59ead6a539e403a3d1f921c2cace40fb
Selim Enes ÖNER <73587934+selimoner34@users.noreply.github.com>
2024-03-22 16:56:45 +0300

    Created course entities. Also created additional entities for own project TaskItem and Store.

---
+++ Core/TestApi.Domain/Entities/Store.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestApi.Domain.Common;
+
+namespace TestApi.Domain.Entities
+{
+    public class Store:EntityBase
+    {
+        public Store()
+        {
+            
+        }
+        public Store(string name, int priorty)
+        {
+            Name = name;
+            Priorty = priorty;
+        }
+
+        public string Name { get; set; }
+        public int Priorty { get; set; }
+        public ICollection<TaskItem> Items { get; set; }
+    }
+}
