6017510ce2eb0dda286fa4a28ff58ed870afaee6
Selim Enes ÖNER <73587934+selimoner34@users.noreply.github.com>
2024-03-25 13:55:43 +0300

    Read Repostiroy Interface and Class implemented.

---
+++ Core/TestApi.Application/Interfaces/Repositories/IWriteRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApi.Application.Interfaces.Repositories
+{
+    public interface IWriteRepository
+    {
+    }
+}
